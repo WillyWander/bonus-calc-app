de12d42663799b794fcf40978ef9ae22d6cd667b
Lillian <a.lillianwilde@gmail.com>
2025-03-22 13:44:36 +0100

    Initial Commit

---
+++ calctest/calctestApp.swift
@@ -0,0 +1,17 @@
+//
+//  calctestApp.swift
+//  calctest
+//
+//  Created by Wilde, Lillian, Dr. on 22.03.25.
+//
+
+import SwiftUI
+
+@main
+struct calctestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calctestTests/calctestTests.swift
@@ -0,0 +1,17 @@
+//
+//  calctestTests.swift
+//  calctestTests
+//
+//  Created by Wilde, Lillian, Dr. on 22.03.25.
+//
+
+import Testing
+@testable import calctest
+
+struct calctestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
